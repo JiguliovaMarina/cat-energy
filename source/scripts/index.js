fd3fe393caebf01994a31db6e417c2b71ebbde75
Marina Jiguliova <m.jiguliova@gmail.com>
2024-08-11 18:03:24 +0300

    work done

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,23 @@
-/* в этот файл добавляет скрипты*/
+const sliderControl = document.querySelector('.slider__control');
+const slider = document.querySelector('.slider');
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+let flag = false;
+
+nav.classList.add('nav--js');
+
+if (sliderControl) {
+  sliderControl.addEventListener('click', () => {
+    if (flag) {
+      slider.classList.toggle('slider--after');
+      slider.classList.toggle('slider--before');
+    } else {
+      slider.classList.add('slider--after');
+      flag = true;
+    }
+  });
+}
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav--open');
+});
